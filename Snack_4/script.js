89b618139521655b40bf1d2fdaed7d3b6e7fa711
AlessandroGallus <algal543@gmail.com>
2021-04-09 12:54:04 +0200

    Correzione 4° snack

--- Snack_4/script.js
+++ Snack_4/script.js
@@ -1,11 +1,11 @@
-var array = [];
+var arr = [];
 var j = 0;
 var numero_utente;
 for (i=0; i<6; i++){
     numero_utente = parseInt(prompt("Inserisci il numero"));
     if (numero_utente%2 != 0){
-        array[j] = numero_utente;
+        arr[j] = numero_utente;
         j++;
     }
 }
-console.log(array[]);
+console.log(arr)
