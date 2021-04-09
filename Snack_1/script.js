eb22e39b324e90052d3d7783a78dde5b3dbc47b7
AlessandroGallus <algal543@gmail.com>
2021-04-09 10:42:07 +0200

    Snack 1 finito

---
+++ Snack_1/script.js
@@ -0,0 +1,23 @@
+var parola_1 = "";
+var parola_2 = "";
+
+parola_1 = prompt("inserisci la prima parola");
+parola_2 = prompt("inserisci la seconda parola");
+
+var l1 = parola_1.length;
+var l2 = parola_2.length;
+
+
+if (l1<l2){
+    console.log("la seconda parola è più lunga della prima: ");
+    console.log(parola_1);
+    console.log(parola_2);
+}else if (l2<l1){
+    console.log("la prima parola è più lunga della seconda: ");
+    console.log(parola_2);
+    console.log(parola_1);
+}else{
+    console.log("le parole hanno la stessa lungezza");
+    console.log(parola_1);
+    console.log(parola_2);
+}
